beee258012559dfed63cd27e6de4291e307e7a70
ahaynie1 <95847697+ahaynie1@users.noreply.github.com>
2022-11-14 00:28:35 -0800

    Remove 0 from single digit hours

--- js/time.js
+++ js/time.js
@@ -3,61 +3,59 @@ Dynamic Data
 ************/
 // define `theClockApp` stored in function to be ran
 const theClockApp = () => {
-  
-// Get and store current date and time with `new Date()` object
-const timeNow = new Date();
-// Check-Check: See all `Date()` methods and properties
-// console.log( timeNow );
+  // Get and store current date and time with `new Date()` object
+  const timeNow = new Date();
+  // Check-Check: See all `Date()` methods and properties
+  // console.log( timeNow );
 
-// Get current hours
-let gotHours = timeNow.getHours();
-// Get current minutes
-let gotMinutes = timeNow.getMinutes();
-// Get current seconds
-let gotSeconds = timeNow.getSeconds();
+  // Get current hours
+  let gotHours = timeNow.getHours();
+  // Get current minutes
+  let gotMinutes = timeNow.getMinutes();
+  // Get current seconds
+  let gotSeconds = timeNow.getSeconds();
 
-// Check-Check: Is the data correct? 
-// console.log(gotHours, gotMinutes, gotSeconds);
+  // Check-Check: Is the data correct?
+  // console.log(gotHours, gotMinutes, gotSeconds);
 
-/************
+  /************
 Format Data
 ************/
-// Get AM or PM
-const gotAmOrPM = gotHours >= 12 ? 'PM' : 'AM';
-// Get 12 hour format
-gotHours = gotHours % 12 || 12;
-// Optionally, append zero to single digit hours
-gotHours = gotHours < 10 ? `0${gotHours}` : gotHours;   
-// Optionally, append zero to single digit minutes
-gotMinutes = gotMinutes < 10 ? `0${gotMinutes}` : gotMinutes;
-// Optionally, append zero to single digit seconds
-gotSeconds = gotSeconds < 10 ? `0${gotSeconds}` : gotSeconds;
+  // Get AM or PM
+  const gotAmOrPM = gotHours >= 12 ? "pm" : "am";
+  // Get 12 hour format
+  gotHours = gotHours % 12 || 12;
+  // Optionally, append zero to single digit hours (REMOVED)
+  gotHours = gotHours < 10 ? `${gotHours}` : gotHours;
+  // Optionally, append zero to single digit minutes
+  gotMinutes = gotMinutes < 10 ? `0${gotMinutes}` : gotMinutes;
+  // Optionally, append zero to single digit seconds
+  gotSeconds = gotSeconds < 10 ? `0${gotSeconds}` : gotSeconds;
 
-/************
+  /************
 Get DOM Elements
 ************/
-// Get Hours
-const hours = document.querySelector(".hours");
-// Get minutes
-const minutes = document.querySelector(".minutes");
-// Get seconds
-const seconds = document.querySelector(".seconds");
-// Get AM or PM
-const amOrPM = document.querySelector('.amOrPM');
+  // Get Hours
+  const hours = document.querySelector(".hours");
+  // Get minutes
+  const minutes = document.querySelector(".minutes");
+  // Get seconds
+  const seconds = document.querySelector(".seconds");
+  // Get AM or PM
+  const amOrPM = document.querySelector(".amOrPM");
 
-/************
+  /************
 Set DOM Elements
 ************/
-// Set the hours
-hours.innerText = gotHours;
-// Set the minutes
-minutes.innerText = gotMinutes;
-// Set the seconds
-seconds.innerText = gotSeconds;
-// Set AM or PM
-amOrPM.innerText = gotAmOrPM;
-  
-}
+  // Set the hours
+  hours.innerText = gotHours;
+  // Set the minutes
+  minutes.innerText = gotMinutes;
+  // Set the seconds
+  seconds.innerText = gotSeconds;
+  // Set AM or PM
+  amOrPM.innerText = gotAmOrPM;
+};
 
 /************
 Run App
