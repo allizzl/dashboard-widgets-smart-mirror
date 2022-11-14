e39ff1dbfb816f6d9d5566a2c007b69bdcb4c400
ahaynie1 <95847697+ahaynie1@users.noreply.github.com>
2022-11-14 00:03:09 -0800

    Replaced API key and added feels like weather

--- js/weather.js
+++ js/weather.js
@@ -1,33 +1,31 @@
-// API key. Replace with your API key
-const APIKEY = 'f2f85762267ef18d0df71e18ec9f3737';
+const APIKEY = "d358027f435b36e2d8fd3af2b1bae2e3";
 // City
-const city = 'Spokane';
+const city = "Spokane";
 // Units for Farenheit
-const units = 'imperial';
+const units = "imperial";
 
 // URL query string
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}`;
 
 // Using fetch to get data
 fetch(url)
-.then( response => response.json() )
-.then( data => {
+  .then((response) => response.json())
+  .then((data) => {
+    // Check-check: Is data good?
+    console.log(data);
 
-  // Check-check: Is data good? 
-  console.log( data );
-  
-  // Get Container for Weather   
-  const weatherContainer = document.querySelector('.weather');
-  
-  // Template to output
-  const template = `
+    // Get Container for Weather
+    const weatherContainer = document.querySelector(".weather");
+
+    // Template to output
+    const template = `
     <h1>Weather</h1>
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
+    <data value="${data.main.feels_like}" class="feelslike">${data.main.feels_like}</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
   `;
-  
-  // Insert dynamic template to container
-  weatherContainer.insertAdjacentHTML("afterbegin", template);
-  
-});
+
+    // Insert dynamic template to container
+    weatherContainer.insertAdjacentHTML("afterbegin", template);
+  });
